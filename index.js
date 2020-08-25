912faa784a5a439d0b3fd6e65341a9f0f657e27e
Witold Urbas <aiur18@student.bth.se>
2020-08-25 14:12:02 +0100

    origin_master

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
 io.on('connection', socket => {
-  socket.on('message', ({ name, message }) => {
-    io.emit('message', { name, message })
+  socket.on('message', ({ name, message, time }) => {
+    io.emit('message', { name, message, time  })
   })
 })
 
-http.listen(4000, function() {
-  console.log('listening on port 4000')
+http.listen(8333, function() {
+  console.log('listening on port 8333')
 })
